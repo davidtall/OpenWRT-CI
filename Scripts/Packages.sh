457bc1637828c1300399478b9228d73072e04d7f
david <269700927@qq.com>
2026-04-25 21:17:48 +0800

    更新Makefile

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -131,8 +131,8 @@ rm -rf ../feeds/luci/applications/luci-app-{passwall*,mosdns,dockerman,dae*,bypa
 rm -rf ../feeds/packages/net/{v2ray-geodata,dae*}
 cp -r $GITHUB_WORKSPACE/package/* ./
 #修复daed/Makefile
-#rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
-#cat luci-app-daed/daed/Makefile
+//rm -rf luci-app-daed/daed/Makefile && cp -r $GITHUB_WORKSPACE/patches/daed/Makefile luci-app-daed/daed/
+cat luci-app-daed/daed/Makefile
 #修复libubox报错
 #sed -i '/include $(INCLUDE_DIR)\/cmake.mk/a PKG_BUILD_FLAGS:=no-werror' ../package/libs/libubox/Makefile
 #sed -i 's|TARGET_CFLAGS += -I$(STAGING_DIR)/usr/include|& -Wno-error=format-nonliteral -Wno-format-nonliteral|' ../package/libs/libubox/Makefile
